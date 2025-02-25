b46f0c8460d0d4eaf6dc126b76f489780989e091
Lester Cheong <lestercheong70+techdev@outlook.com>
2025-02-26 03:21:10 +0800

    Refactor Home component to remove unused styles and integrate global CSS for improved styling consistency

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { routes } from "../router/router";
-import "../styles/App.css";
 
 interface AppProps {
   title?: string;
@@ -7,28 +6,14 @@ interface AppProps {
 
 export const Home = ({ title }: AppProps) => {
   return (
-    <div className="App-header">
-      <h1 className="bg-slate-600 rounded-md flex">{title}</h1>
-      <img src={"/logo.svg"} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-      <a style={{}} href="https://www.google.com" className="bg-black text-white">
-        go to google
-      </a>
-      <hr />
-      <div>
-        {routes.map((route) => {
-          return (
-            <a href={route.routeObject.path}>
-              <button className="bg-blue-500 text-white mx-2 px-3 py-[100px] min-w-[150px] rounded-md font-semibold">{route.title}</button>
-            </a>
-          );
-        })}
-      </div>
+    <div>
+      {routes.map((route) => {
+        return (
+          <a href={route.routeObject.path}>
+            <button className="bg-blue-500 text-white mx-2 px-3 py-[100px] min-w-[150px] rounded-md font-semibold">{route.title}</button>
+          </a>
+        );
+      })}
     </div>
   );
 };
