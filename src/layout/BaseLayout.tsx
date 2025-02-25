f5842eddc968dadd353df073c023d1c2e9ba72e5
greg <gregorytann10@gmail.com>
2025-02-25 21:36:24 +0800

    Refactor BaseLayout component for improved navigation styling and structure

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -11,14 +11,14 @@ const BaseLayout = ({ children }: BaseLayoutProps) => {
         <nav className="flex overflow-x-auto whitespace-nowrap no-scrollbar p-4">
           {routes.map((route) => {
             return (
-              <a className="bg-blue-500 text-white mx-2 p-1" href={route.routeObject.path}>
+              <a className="bg-blue-500 text-white mx-2 px-3 py-1 min-w-[150px] rounded-md font-semibold" href={route.routeObject.path}>
                 {route.title}
               </a>
             );
           })}
         </nav>
       </header>
-      <div>{children}</div>
+      {children}
       <footer>{/* Footer content */}</footer>
     </>
   );
