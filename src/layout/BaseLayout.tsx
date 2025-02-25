2241e5d1ce84cccbc8e4b43bbc705fa4f4e3edc8
greg <gregorytann10@gmail.com>
2025-02-25 21:51:21 +0800

    Enhance BaseLayout to conditionally display navigation based on route; update Home component to include route buttons

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,22 +1,37 @@
-import { routes } from "../router/router";
+import { useLocation } from "react-router-dom";
+import { ROUTER_PATHS, routes } from "../router/router";
+import { useEffect, useState } from "react";
 
 interface BaseLayoutProps {
   children: React.ReactNode;
 }
 
 const BaseLayout = ({ children }: BaseLayoutProps) => {
+  const location = useLocation();
+  const [shouldDisplayNav, setShouldDisplayNav] = useState<boolean>(true);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case ROUTER_PATHS.HOME:
+        setShouldDisplayNav(false);
+        break;
+    }
+  }, [location]);
+
   return (
     <>
       <header>
-        <nav className="flex overflow-x-auto whitespace-nowrap no-scrollbar p-4">
-          {routes.map((route) => {
-            return (
-              <a className="bg-blue-500 text-white mx-2 px-3 py-1 min-w-[150px] rounded-md font-semibold" href={route.routeObject.path}>
-                {route.title}
-              </a>
-            );
-          })}
-        </nav>
+        {shouldDisplayNav ? (
+          <nav className="flex overflow-x-auto whitespace-nowrap no-scrollbar p-4">
+            {routes.map((route) => {
+              return (
+                <a className="bg-blue-500 text-white mx-2 px-3 py-1 min-w-[150px] rounded-md font-semibold" href={route.routeObject.path}>
+                  {route.title}
+                </a>
+              );
+            })}
+          </nav>
+        ) : null}
       </header>
       {children}
       <footer>{/* Footer content */}</footer>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { routes } from "../router/router";
 import "../styles/App.css";
 
 interface AppProps {
@@ -6,20 +7,28 @@ interface AppProps {
 
 export const Home = ({ title }: AppProps) => {
   return (
-    <div className="">
-      <header className="App-header">
-        <h1 className="bg-slate-600 rounded-md flex">{title}</h1>
-        <img src={"/logo.svg"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-        <a style={{}} href="https://www.google.com" className="bg-black text-white">
-          go to google
-        </a>
-      </header>
+    <div className="App-header">
+      <h1 className="bg-slate-600 rounded-md flex">{title}</h1>
+      <img src={"/logo.svg"} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <a style={{}} href="https://www.google.com" className="bg-black text-white">
+        go to google
+      </a>
+      <hr />
+      <div>
+        {routes.map((route) => {
+          return (
+            <a href={route.routeObject.path}>
+              <button className="bg-blue-500 text-white mx-2 px-3 py-[100px] min-w-[150px] rounded-md font-semibold">{route.title}</button>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 };
